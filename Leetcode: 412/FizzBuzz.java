1f4849ba22fc83874924684b7abb2a9961348b81
Harsh Pandey <harsh@Harshs-MacBook-Air.local>
2026-07-20 01:55:16 +0530

    new question

---
+++ Leetcode: 412/FizzBuzz.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> l = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            if(i % 3 == 0 && i % 5 == 0) l.add("FizzBuzz");
+            else if(i % 3 == 0) l.add("Fizz");
+            else if(i % 5 == 0) l.add("Buzz");
+            else l.add(Integer.toString(i));
+        }
+        return l;
+    }
+}
